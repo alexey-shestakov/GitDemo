c500cfd24b9273c6271b4e76eced80a0b3b3c528
Алексей Шестаков <shestakad@gmail.com>
2022-11-06 16:31:30 +0300

    initial commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Alexey Shestakov on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
